df957e2cc6f1e936cf31348e2829a145aea8bdae
VSharshukov <vsharshukov@gmail.com>
2016-05-19 22:35:43 +0300

    0.0.11 get_number() doesn't work

--- agym_bot/Bot/bot.py
+++ agym_bot/Bot/bot.py
@@ -1,13 +1,32 @@
+from Functions import db
 import telebot
 import config
 
-def listener(messages):
-	for m in messages:
-		if m.content_type == 'text':
-			bot.send_message(m.chat.id, m.text)
+bot = telebot.TeleBot(config.token)
 
-if __name__ == '__main__':
-	bot = telebot.TeleBot(config.token)
-	# bot.remove_webhook()
-	bot.set_update_listener(listener)
-	bot.polling(none_stop=True)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, 'Hello')
+
+
+@bot.message_handler(commands=['delete_me'])
+def delete_user(message):
+	bot.send_message(message.chat.id, 'ня.пока')
+
+
+@bot.message_handler(commands=['get_number'])
+def get_mobile(message):
+	if message.text:
+		if len(message.text.split(' ')) == 2:
+			db.get_mobile.group(message.text.split(' ')[1])
+
+
+@bot.message_handler(content_types=['text', 'sticker'])
+def repeat_all_messages(message):
+	if message.text:
+		bot.send_message(message.chat.id, message.text)
+	else:
+		bot.send_message(message.chat.id, ';')
+
+bot.polling()
